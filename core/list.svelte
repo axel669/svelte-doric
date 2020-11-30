0ee1a9c33a9ef36ab0b81a0d24a5be9a4616de1c
Axel669 <haryuu.hanekata@gmail.com>
2020-11-29 23:01:51 -0800

    remove old code, make default item unnamed slot

--- core/list.svelte
+++ core/list.svelte
@@ -1,31 +1,11 @@
-<!-- <script context="module">
-import ListItem from "./list/item.svelte"
-import ListHeader from "./list/header.svelte"
-import ListItemContent from "./list/content.svelte"
-
-export {ListItem, ListHeader, ListItemContent}
-;</script> -->
-
 <script>
-    // import {createEventDispatcher} from "svelte"
     import Divider from "./divider.svelte"
 
     export let items = []
-    // export let clickable
     export let height
     export let compact
     let klass = ""
     export {klass as class}
-
-    // const dispatch = createEventDispatcher()
-    // const itemClick = evt => {
-    //     if (clickable !== true) {
-    //         return
-    //     }
-    //     dispatch("itemClick", items[evt.currentTarget.dataset.index].item)
-    // }
-
-    // ;$: styleText = height ? `height: ${height}; ${style}` : style
 ;</script>
 
 <style>
@@ -76,10 +56,6 @@ export {ListItem, ListHeader, ListItemContent}
     doric-list > :global(list-item[control]) {
         padding: 0px;
     }
-
-    list-item {
-        border: 1px solid blue;
-    }
 </style>
 
 <doric-list class:compact class={klass} style="--list-height: {height}">
@@ -93,7 +69,7 @@ export {ListItem, ListHeader, ListItemContent}
                 </list-header>
             </slot>
         {:else}
-            <slot name="item" {item}>
+            <slot {item}>
                 <list-item>
                     <list-item-content>
                         {item ?? ""}
