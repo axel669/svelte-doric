b5b83ad187a9c6329161d8dd68655df2c7deef4c
Axel669 <haryuu.hanekata@gmail.com>
2020-12-03 00:05:30 -0800

    started tron theme

---
+++ core/theme/tron.svelte
@@ -0,0 +1,40 @@
+<script>
+    import {css} from "../style/css.js"
+
+    const theme = css`
+        body {
+            --font: Inconsolata;
+            --background: #030303;
+            --background-layer: #080808;
+            --background-highlight: #171717;
+            --layer-border-width: 1px;
+
+            --ripple-dark: #00000060;
+            --ripple-light: #FFFFFF60;
+            --text-light: white;
+            --text-dark: black;
+
+            --primary: #00aaff;
+            --primary-ripple: #00aaff60;
+            --secondary: #2fbc2f;
+            --secondary-ripple: #2fbc2f60;
+            --danger: #df5348;
+            --danger-ripple: #df534860;
+
+            --text-normal: var(--text-light);
+            --text-secondary: #a0a0a0;
+            --text-invert: var(--text-dark);
+
+            --text-size: 14px;
+            --text-size-title: 18px;
+            --text-size-header: 16px;
+            --text-size-info: 13px;
+            --text-size-secondary: 12px;
+
+            --ripple-normal: var(--ripple-light);
+            --ripple-invert: var(--ripple-dark);
+        }
+    `
+</script>
+
+{@html theme}
