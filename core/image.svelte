ca6125e72ff8679c03fe4bf9d7d4621c82d649f5
Axel669 <haryuu.hanekata@gmail.com>
2021-07-01 17:29:23 -0700

    make Image a wrapper for the img tag without extra elements

--- core/image.svelte
+++ core/image.svelte
@@ -6,29 +6,26 @@
     export let height
     export let width
     export let fit = "contain"
+    export let float
 
-    $: imageSize = {
+    $: props = {
+        float,
         "image-width": width,
         "image-height": height,
-    }
-    $: imageFit = {
         "image-contain": fit,
     }
 </script>
 
 <style>
 img {
-    object-fit: var(--image-contain);
     width: 100%;
     height: 100%;
-}
-image-container {
-    display: inline-block;
+
+    float: var(--float, none);
+    object-fit: var(--image-contain);
     width: var(--image-width);
     height: var(--image-height);
 }
 </style>
 
-<image-container use:vars={imageSize}>
-    <img src={source} {alt} use:vars={imageFit} />
-</image-container>
+<img src={source} {alt} use:vars={props} />
