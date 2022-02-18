b963d0820a59f90cc50131f65fb18424b429871c
Axel669 <haryuu.hanekata@gmail.com>
2022-02-17 22:49:29 -0800

    added control drawer for neat components that arent menus

---
+++ core/control-drawer.svelte
@@ -0,0 +1,34 @@
+<script>
+    import Modal from "./modal.svelte"
+
+    export let open = false
+    export let persistent = false
+
+    const drawerSlide = (node, options) => {
+        return {
+            delay: 0,
+            duration: 250,
+            css: (t, u) => `
+                transform: translateX(${u * 100}%);
+                opacity: ${t};
+            `
+        }
+    }
+</script>
+
+<style>
+    control-drawer {
+        position: absolute;
+        top: 0px;
+        right: 0px;
+        height: 100vh;
+        min-width: 25vw;
+        background-color: var(--card-background);
+    }
+</style>
+
+<Modal bind:open on:close {persistent}>
+    <control-drawer transition:drawerSlide>
+        <slot />
+    </control-drawer>
+</Modal>
