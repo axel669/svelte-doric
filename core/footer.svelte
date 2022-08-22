673cc52fd1b87b878fb19c39306a56a60d9b9983
Axel669 <haryuu.hanekata@gmail.com>
2022-08-22 15:33:56 -0700

    add bordered option to match with paper cards

--- core/footer.svelte
+++ core/footer.svelte
@@ -1,4 +1,5 @@
 <script>
+    export let bordered = false
 </script>
 
 <style>
@@ -7,8 +8,13 @@
         grid-template-columns: max-content auto max-content;
         box-shadow: 0px -2px 4px rgba(0, 0, 0, 0.25);
         height: 56px;
+        gap: 2px;
+        padding: 2px;
 
         background-color: var(--card-background);
+    }
+    doric-footer.bordered {
+        border: 1px solid var(--text-normal);
     }
 
     footer-area {
@@ -16,7 +22,7 @@
     }
 </style>
 
-<doric-footer>
+<doric-footer class:bordered>
     <footer-area>
         <slot name="left" />
     </footer-area>
