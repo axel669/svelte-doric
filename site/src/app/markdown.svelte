f83c17b83c637b563cb0e9cb66d9796fabd68a95
Axel669 <haryuu.hanekata@gmail.com>
2022-10-17 22:42:32 -0700

    make custom markdown component to highlight code blocks better

---
+++ site/src/app/markdown.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { onMount } from "svelte"
+
+    import Markdown from "svelte-markdown"
+
+    export let docs
+
+    onMount(
+        () => {
+            const codez = document.querySelectorAll("pre")
+            for (const code of codez) {
+                hljs.highlightBlock(code)
+            }
+        }
+    )
+</script>
+
+<Markdown source={docs} />
